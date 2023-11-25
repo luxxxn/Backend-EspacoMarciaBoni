7015cec484a6fa59c21ae1f6117df2c96c3ef849
luxxxn <152024158+luxxxn@users.noreply.github.com>
2023-11-25 17:42:09 +0000

    feats: modelo de manicure

---
+++ Modelo/manicure.js
@@ -0,0 +1,91 @@
+import PizzaDAO from "../Persistencia/pizzaDAO.js";
+
+export default class Manicure{
+    #codigo;
+    #descricao;
+    #opcoes;
+    #valor;
+    #urlImagem;
+
+    constructor(codigo, descricao, opcoes, valor, urlImagem){
+        this.#codigo = codigo;
+        this.#descricao = descricao;
+        this.#opcoes = opcoes;
+        this.#valor = valor;
+        this.#urlImagem = urlImagem;
+    }
+
+    get codigo(){
+        return this.#codigo;
+    }
+
+    set codigo(novoCodigo){
+        this.#codigo = novoCodigo;
+    }
+
+    get descricao(){
+        return this.#descricao;
+    }
+
+    set descricao(novaDescricao){
+        this.#descricao = novaDescricao;
+    }
+
+    get opcoes(){
+        return this.#opcoes;
+    }
+
+    set opcoes(novoopcoes){
+        this.#opcoes = novoopcoes;
+    }
+
+    get valor(){
+        return this.#valor;
+    }
+
+    set valor(novoValor){
+        this.#valor = novoValor;
+    }
+
+
+    get urlImagem(){
+        return this.#urlImagem;
+    }
+
+    set urlImagem(novaUrl){
+        this.#urlImagem = novaUrl;
+    }
+
+    //override do método toJSON()
+    toJSON(){
+        return {
+            'codigo': this.#codigo,
+            'descricao': this.#descricao,
+            'opcoes': this.#opcoes,
+            'valor': this.#valor,
+            'urlImagem': this.#urlImagem,
+        }
+    }
+
+    async gravar(){
+        //DAO = Data Access Object
+        const pizzaDAO = new PizzaDAO();
+        await pizzaDAO.gravar(this);
+    }
+
+    async atualizar(){
+        const pizzaDAO = new PizzaDAO();
+        await pizzaDAO.atualizar(this);
+    }
+
+    async excluir(){
+        const pizzaDAO = new PizzaDAO();
+        await pizzaDAO.excluir(this);
+    }
+
+    async consultar(){
+        const pizzaDAO = new PizzaDAO();
+        return await pizzaDAO.consultar();
+    }
+
+}
